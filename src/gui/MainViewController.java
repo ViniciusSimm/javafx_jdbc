50ca620a67d8bcbb5b8c90fd382184a9c39ac84a
Vinicius Sylvestre <vinicius.sylvestre@hypeone.com>
2023-10-24 10:43:33 -0300

    department view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("a");
+		loadView("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
